7a23a1a16dd6f0697fb4a045634dff6d4edd1aa5
Kenny Nguyen <kkenny.nguyen@gmail.com>
2020-10-20 01:17:12 +1100

    jsx-2 init

---
+++ jsx-2/src/index.js
@@ -0,0 +1,24 @@
+// Import the React and React Dom libraries
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Create a React component
+const App = function () {
+  const buttonText = { text: "Click me" };
+  const labelText = "Enter name:";
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
+};
+
+// Take the React component and display on screen
+ReactDOM.render(<App />, document.querySelector("#root"));
